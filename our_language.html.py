daa97288873dad7a7fc880dfe0ea539ed04a7bf1
Ana Karen <anakarenbmak@yahoo.com>
2019-02-28 20:32:39 -0600

    Modify sample code

--- our_language.html.py
+++ our_language.html.py
@@ -1,48 +1,147 @@
-%str hello = "Hello I am a global variable"
-%eval(hello)
+program the_name_of_the_program
 
+    #This is a comment
 
-%console
-    def friends_function(list friends): # RECIEVE PARAMETERS
-      int max = 40
-      str hello2 = "I am a local variable"
-      if friends.length > max:
-          friends.push("byte") #REMOVE ELM FROM LIST
-        else if friends.length < max: #ADD ELM TO LIST
-            friends.pop()
-        return friends
+    -script {
+        # VARIABLE AND DECLARATION AND INITIALIZATION
 
-    list[str] my_friends_list = ["ana karen", "sergio", "laura"] #LIST
-    my_friends_list = friends_function(friends) # SEND PARAMS
-    eval("I have" + my_friends_list.length() + "friends !!!") #W RITE
+        # You can declare global variables like this.
+        str = "I am a global variable"
+        # We support different data types and you can define them like this
+        str name = "Ana Karen"
+        int age = 23
+        double height = 1.6
+        char sex = 'F'
+        bool legal # This is how you deine an uninitialized variable
 
--h1 "class: none"
-    This is a Title
+        # Slices and 2D Slices work as arrays and matrixes respectively
+        my_slice [2] char = ['a','b']
+        my_2D_slice [3][3] int = [[]]
 
--h2 "class: header-2-class"
-    And this is the subtitle
+        # You can add and modify elements of your array/matrix
+        my_slice[0] = 'z' #this will result in the array being ['z', 'b']
+        my_2D_slice[0][0] = 1
 
--div
-    -p
-    What we want to do here is being able to let others write pythoninc code while being able to
-    demonstrate their findings.
+        # CONDITIONALS
 
-    -p
-    We noticed that a lot of researches and financial people are using python
-    so we tought that we could do a language to help them publish their results
-    faster and with a minimal learning curve. The idea is that,
-    if you know python, you know how to write using our language.
+        # You can create a simple condition like the following
+        if (sex is 'F') {
+            eval("Female!")
+        }
 
-%sort_list(list[type], criteria) #does not include dict or list[list]
-# criteria = min/max
-# type = int, double, str, char, bool
+        if (sex not 'F') {
+            eval("Male!")
+        }
 
-%sort_mat([i][j], criteria, i)
+        # You can add an else to your condition
+        if (age <= 18) {
+            legal = True
+            eval("Legal! :)")
+        } else {
+            legal = False
+            eval("Underage :(")
+        }
 
-%median(list[type]) #type = int, double
-%mode(list[type]) #type = int, double, str, char, bool
-%avg(list[type]) #type = int, double
-%pow(type a, int b) #type = int, double
+        # LOOPS
 
+        #You can loop through an array/matrix or a fixed number of times by using a loop
+        # This is a while disguised as a loop
+        int i = 1
+        loop (i not 10) {
+            eval("Loop number" + i + "")
+            # Please don't declare any new variable inside a loop.
+            # int hola = 1 
+        }
 
-# list = array
+        # MODULES
+
+        # This is how you declare a function.
+        # The group of parentheses is used for parametrization and whatever comes after the ':' the return type.
+        # You can use the spit statement to return your values
+        def sum (int a, int b) : int {
+            int result = a + b #This is a local variable
+            spit result
+        }
+
+        # This is how you declare a non-spitting function (aka void)
+        def greeting (str nickname) : {
+            eval("Hello " + nickname + "!")
+        }
+
+        # BASIC STATISTICS FUNCTIONS
+
+        #oh yes we are very cool and we support some basic statistical functions
+
+        # You can even import a table from an excel sheet into a matrix!
+        # suck_csv receives an argument with the path to your csv file
+        my_table [2][3] int = suck_csv("C:\\Documents\car.csv")
+
+        # You can manipulate your slices by using some the default methods
+        test_slice [5] int = [4,3,1,7,0]
+        sort_slice(test_slice, criteria)
+            #type = int, double, str, char, bool
+            # criteria = min/max
+
+        # Get valuable insight from your data
+        median(test_slice) # will spit => 3
+            #type = int, double.
+        mode(test_slice) # would spit => 3
+            #type = int, double, str, char, bool
+        avg(test_slice) # would spit => 3
+            #type = int, double
+
+        #Supported math function
+        pow(type a, int b)
+            #type = int, double
+
+        # You can manipulate your sort_2D_slice by using some the default methods
+        sort_2D_slice(my_table, criteria, column_index)
+            # criteria = min/max
+    
+    }
+
+    h1 { "This is a Title" }
+    h2 {
+        class: "header-2-class"
+        "And this is the subtitle"
+    }
+
+    div {
+        p { "What we want to do here is being able to let others write pythoninc 
+            code while being able to demonstrate their findings."
+        }
+        p {
+            <^ "Whatever goes inside this will be evaluated as code." ^>
+            "The result of 1+1 is" <^ 1+1 ^>
+        }
+        p {
+            We noticed that a lot of researches and financial people are using python
+            so we tought that we could do a language to help them publish their results
+            faster and with a minimal learning curve. The idea is that,
+            if you know python, you know how to write using our language.
+        }
+    }
+    
+    div {
+        class: "example-class"
+        
+        table {
+            tr {
+                th {"Brand"}
+                th {"Year"}
+                th {"Model"}
+            }
+            tr {
+                script{
+                    int counter = 0
+                    loop (counter not len(my_table)) {
+                        th {
+                            eval("Loop number" + i + "")
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+    
