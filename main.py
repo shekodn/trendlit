7cc602b1cd68b01e48a9c504fbc6edf74548a008
shekodn <sdiazn19@gmail.com>
2019-03-21 19:57:26 -0600

    Sync modules

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-
 import sys
 from parser.parser import parser, yacc
+from semantic_cube.semantic_cube import (
+    Cube,
+    type_int,
+    type_double,
+    type_bool,
+    type_error,
+    type_none,
+)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
