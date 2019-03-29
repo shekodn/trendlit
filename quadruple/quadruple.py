2730131e7f615183fe5a60a1ca75aeb8c8dd11cd
shekodn <sdiazn19@gmail.com>
2019-03-28 18:01:56 -0600

    Add method to Quadruple

--- quadruple/quadruple.py
+++ quadruple/quadruple.py
@@ -6,4 +6,15 @@ class Quadruple(object):
         self.oper3 = oper3
 
     def __str__(self):
-        return "%d %d %s %d" % (self.op, self.oper1, self.oper2, self.oper3)
+        return "%d %d %d %d" % (self.op, self.oper1, self.oper2, self.oper3)
+
+    def is_quadruple(self):
+        my_int = 666
+        a = type(self.op) == type(my_int)
+        b = type(self.oper1) == type(my_int)
+        c = type(self.oper2) == type(my_int)
+        d = type(self.oper3) == type(my_int)
+
+        if a and b and c and d:
+            return True
+        return False
