f2b0d6d25addc123b15caad73cb2009bd4f77085
shekodn <sdiazn19@gmail.com>
2019-03-30 11:49:24 -0600

    Refactor tests

--- tests/test_tl_files.py
+++ tests/test_tl_files.py
@@ -1,6 +1,8 @@
 import unittest  # Reference: https://docs.python.org/2/library/unittest.html
 from parser.parser import parser, yacc, procedure_directory, quad_helper, error_helper
+
 TESTING_PREFIX = "tests/tl_test_files/"
+
 
 def aux_tl_file(file_name, expected_number_of_errors):
     try:
@@ -9,44 +11,21 @@ def aux_tl_file(file_name, expected_number_of_errors):
         f.close()
         yacc.parse(data, tracking=True)
         return error_helper.error_cont
-        # if (expected_number_of_errors is error_helper.error_cont):
-        #     return [True, expected_number_of_errors]
-        # else:
-        #     actual_number_of_errors = error_helper.error_cont
-        #     return [False, actual_number_of_errors]
-
-
     except EOFError:
         print("EOFError")
+
 
 class OurTestCase(unittest.TestCase):
     def test_tl_file_estatutos_secuenciales_0(self):
         file_name = TESTING_PREFIX + "estatutos_secuenciales_0.tl"
         expected_errors = 0
-        result =  aux_tl_file(file_name, expected_errors)
+        result = aux_tl_file(file_name, expected_errors)
         print(f"\nTESTING: {file_name}\n")
         self.assertEqual(result, expected_errors)
 
-    def test_tl_file_estatutos_secuenciales_1(self):
-        file_name = TESTING_PREFIX + "pass_estatutos_secuenciales_0.tl"
-        expected_errors = 0
-        result =  aux_tl_file(file_name, expected_errors)
-        print(f"\nTESTING: {file_name}\n")
-        self.assertEqual(result, expected_errors)
-
-    def test_tl_file_estatutos_secuenciales_2(self):
+    def test_tl_file_fail_estatutos_secuenciales_1(self):
         file_name = TESTING_PREFIX + "fail_estatutos_secuenciales_1.tl"
         expected_errors = 1
-        result =  aux_tl_file(file_name, expected_errors)
+        result = aux_tl_file(file_name, expected_errors)
         print(f"\nTESTING: {file_name}\n")
         self.assertEqual(result, expected_errors)
-
-
-
-
-    # def test_fail_estatutos_secuenciales_1(self):
-    #     file_name = TESTING_PREFIX + "fail_estatutos_secuenciales_1.tl"
-    #     expected_errors = 0
-    #     result =  aux_tl_file(file_name, expected_errors)
-    #     print(f"\nTESTING: {file_name}\n")
-    #     self.assertEqual(result, True)
