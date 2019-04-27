38b96e0be76dbab26d903e6cef65675dc3f94226
shekodn <sdiazn19@gmail.com>
2019-04-27 17:33:19 -0500

    Change m for M in script

--- bump_version.sh
+++ bump_version.sh
@@ -4,4 +4,4 @@ set -e
 current_version=$(grep RELEASE?= ./Makefile | sed -e 's/RELEASE?=\(.*\)/\1/g')
 next_version=$(echo $current_version | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
 
-sed -i -e "s/\(RELEASE?=\).*/\1$next_version/" makefile
+sed -i -e "s/\(RELEASE?=\).*/\1$next_version/" Makefile
