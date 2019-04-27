1651b0cbbec2303100db234dbfc38b7c7b1b9a58
shekodn <sdiazn19@gmail.com>
2019-04-27 16:59:44 -0500

    Add bump version to handle docker hub images

---
+++ bump_version.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+
+current_version=$(grep RELEASE?= ./Makefile | sed -e 's/RELEASE?=\(.*\)/\1/g')
+next_version=$(echo $current_version | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
+
+sed -i -e "s/\(RELEASE?=\).*/\1$next_version/" makefile
