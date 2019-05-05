722ee27f7e3783f7d7698e97c7c1b8f19ed08e8b
Ana Karen <anakarenbmak@yahoo.com>
2019-05-05 01:40:54 -0500

    Fix vm memory

--- tests/test_compiled_code.py
+++ tests/test_compiled_code.py
@@ -22,3 +22,14 @@ class QuadruplesTestCase(unittest.TestCase):
         result = filecmp.cmp(expected_file, expected_file)
         should_files_be_equal = True
         self.assertEqual(should_files_be_equal, result)
+
+    def test_9_2_pass_array_access(self):
+        tl_file_to_compile = "9_2_pass_array_access.tl"
+        trendlit_helper.reads_file(
+            COMPILED_CODE_DIR, OUR_TESTS_PATH + tl_file_to_compile
+        )
+        expected_file = TESTING_CODE_DIR + tl_file_to_compile + ".html.test"
+        generated_file = COMPILED_CODE_DIR + tl_file_to_compile + ".html"
+        result = filecmp.cmp(expected_file, generated_file)
+        should_files_be_equal = True
+        self.assertEqual(should_files_be_equal, result)
