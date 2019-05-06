5290a3b0a4e782a970fc066bd657118b2c4e498c
shekodn <sdiazn19@gmail.com>
2019-05-06 17:35:55 -0500

    Refactors server

--- server/test.py
+++ server/test.py
@@ -7,16 +7,16 @@ import cgi, cgitb
 form = cgi.FieldStorage()
 
 # Get data from fields
-if form.getvalue('textcontent'):
-   text_content = form.getvalue('textcontent')
+if form.getvalue("textcontent"):
+    text_content = form.getvalue("textcontent")
 else:
-   text_content = "Not entered"
+    text_content = "Not entered"
 
-print ("Content-type:text/html\r\n\r\n")
-print ("<html>")
-print ("<head>")
-print ("<title>Text Area - Fifth CGI Program</title>")
-print ("</head>")
-print ("<body>")
-print ("<p> %s </p>" % text_content)
-print ("</body>")
+print("Content-type:text/html\r\n\r\n")
+print("<html>")
+print("<head>")
+print("<title>Text Area - Fifth CGI Program</title>")
+print("</head>")
+print("<body>")
+print("<p> %s </p>" % text_content)
+print("</body>")
--- server/upload.py
+++ server/upload.py
@@ -4,6 +4,9 @@
 import cgi, cgitb, sys, os
 from parser.parser import parser, yacc, quad_helper, error_helper, parser_helper, memory
 from virtual_machine.virtual_machine import run_code, vmh
+from trendlit_helper.trendlit_helper import TrendlitHelper
+
+trendlit_helper = TrendlitHelper()
 
 
 cgitb.enable()
@@ -14,11 +17,6 @@ form = cgi.FieldStorage()
 fileitem = form["filename"]
 
 print(f"Content-type:text/html\n")
-print("<html>")
-print("<head>")
-print("<title> Trendlit - Cloud Based Programming Language</title>")
-print("</head>")
-print("<body>")
 
 # Test if the file was uploaded
 if fileitem.filename:
@@ -27,28 +25,9 @@ if fileitem.filename:
     fn = os.path.basename(fileitem.filename)
     open("/tmp/" + fn, "wb").write(fileitem.file.read())
     message = 'The file "' + fn + '" was uploaded successfully\n'
-
-    try:
-        with open("/tmp/" + fn, "r") as file:
-            data = file.read()
-
-        yacc.parse(data, tracking=True)
-
-        if error_helper.error_cont is 0:
-            run_code(quad_helper.queue_quad, memory.constant_values)
-            for result in vmh.queue_results:
-                print(result)
-        else:
-            print(f"Your code doesn't compile. Please try harder \n")
-            print(f"# of Errors: {error_helper.error_cont}\n")
-            error_helper.print_errors()
-    except:
-        message = f"Error. We couldn't compile successfully.\n"
-        print(message)
+    with open("/tmp/" + fn, "r") as file:
+        data = file.read()
+    trendlit_helper.run(data)
 else:
     message = "No file was uploaded\n"
     print(message)
-
-
-print("</body>")
-print("</html>")
