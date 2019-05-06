45cd36c5827cf198b3afca820b276e73068fcc0f
shekodn <sdiazn19@gmail.com>
2019-05-05 20:04:36 -0500

    Adds final find func

--- tests/test_compiled_code.py
+++ tests/test_compiled_code.py
@@ -110,3 +110,14 @@ class QuadruplesTestCase(unittest.TestCase):
         result = filecmp.cmp(expected_file, generated_file)
         should_files_be_equal = True
         self.assertEqual(should_files_be_equal, result)
+
+    def test_final_function_find(self):
+        tl_file_to_compile = "final_function_find.tl"
+        trendlit_helper.reads_file(
+            COMPILED_CODE_DIR, OUR_TESTS_PATH + tl_file_to_compile
+        )
+        expected_file = TESTING_CODE_DIR + tl_file_to_compile + ".html.test"
+        generated_file = COMPILED_CODE_DIR + tl_file_to_compile + ".html"
+        result = filecmp.cmp(expected_file, generated_file)
+        should_files_be_equal = True
+        self.assertEqual(should_files_be_equal, result)
