37a7a31b37d18ed5729cbd5907ac7b828275cca4
shekodn <sdiazn19@gmail.com>
2019-05-24 11:47:48 -0500

    Add latest tag

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -10,3 +10,4 @@ HASH=`git log --format="%H" -n 1 | cut -c1-6`
 
 docker build --build-arg RELEASE=${TAG} -t ${REPOSITORY}/${IMAGE}:${TAG} .
 docker build --build-arg RELEASE=${TAG} -t ${REPOSITORY}/${IMAGE}:${HASH} .
+docker tag ${REPOSITORY}/${IMAGE}:${HASH} ${REPOSITORY}/${IMAGE}:latest
